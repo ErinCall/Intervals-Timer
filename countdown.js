bfc708218c6efd6ef059d83d27883a87aada0b14
Andrew Lorente <andrew.lorente@gmail.com>
2010-11-13 18:47:31 -0800

    Countdown script that can toggle on and off.

--- countdown.js
+++ countdown.js
@@ -1,3 +1,10 @@
 function decrementIt() {
-	$('countdowner')
+	var currentTime = jQuery.trim($('#countdowner').html())
+	if (currentTime == 0 ) {
+		intervalToggle();
+		currentTime = 10;
+	} else {
+		currentTime--;
+	}
+	$('#countdowner').html(currentTime);
 }
--- screen_color.js
+++ screen_color.js
@@ -1,3 +1,14 @@
 function setBodyColor(color) {
 	document.body.bgColor = color;
 }
+
+var start = 1;
+function intervalToggle() {
+	if ( start ) {
+		setBodyColor('#ff0000');
+		start = 0;
+	} else {
+		setBodyColor('#0000ff');
+		start = 1;
+	}
+}
