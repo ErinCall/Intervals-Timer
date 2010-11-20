0a1c0f5120956c6e8532f7773d2f092a33def23e
Andrew Lorente <andrew.lorente@gmail.com>
2010-11-20 08:43:40 -0800

    Use spaces instead of hardtabs.

--- countdown.js
+++ countdown.js
@@ -1,26 +1,26 @@
 var currentTime = 40;
 function tick() {
-	if (currentTime == 0 ) {
-		intervalToggle();
-		currentTime = 2*60*10;
-	} else {
-		currentTime--;
-	}
+    if (currentTime == 0 ) {
+        intervalToggle();
+        currentTime = 2*60*10;
+    } else {
+        currentTime--;
+    }
 }
 
 function displayTime() {
-	var deciseconds = Math.floor(currentTime % 10);
-	var seconds = Math.floor(currentTime/10) % 60;
-	var minutes = (Math.floor(currentTime/(60*10))) % 60;
-	$('#countdown').html(sprintf(
-		'%02d:%02d.%d', minutes, seconds, deciseconds
-	));
+    var deciseconds = Math.floor(currentTime % 10);
+    var seconds = Math.floor(currentTime/10) % 60;
+    var minutes = (Math.floor(currentTime/(60*10))) % 60;
+    $('#countdown').html(sprintf(
+        '%02d:%02d.%d', minutes, seconds, deciseconds
+    ));
 }
 
 $(document).ready(function() {
-	displayTime();
-	window.setInterval(function() {
-		tick();
-		displayTime();
-	}, 100)
+    displayTime();
+    window.setInterval(function() {
+        tick();
+        displayTime();
+    }, 100)
 });
--- screen_color.js
+++ screen_color.js
@@ -1,14 +1,14 @@
 function setBodyColor(color) {
-	$('#outer').css('background-color', color);
+    $('#outer').css('background-color', color);
 }
 
 var start = 1;
 function intervalToggle() {
-	if ( start ) {
-		setBodyColor('#ff0000');
-		start = 0;
-	} else {
-		setBodyColor('#0000ff');
-		start = 1;
-	}
+    if ( start ) {
+        setBodyColor('#ff0000');
+        start = 0;
+    } else {
+        setBodyColor('#0000ff');
+        start = 1;
+    }
 }
