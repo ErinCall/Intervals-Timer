297855c4bd2aeea296070c2dbc5b54bb7875cb97
Andrew Lorente <andrew.lorente@gmail.com>
2010-11-19 19:38:13 -0800

    Refactor countdown-updating and displaying code.

--- countdown.js
+++ countdown.js
@@ -1,10 +1,21 @@
-function decrementIt() {
-	var currentTime = jQuery.trim($('#countdown').html())
+var currentTime = 4;
+function tick() {
 	if (currentTime == 0 ) {
 		intervalToggle();
 		currentTime = 10;
 	} else {
 		currentTime--;
 	}
+}
+
+function displayTime() {
 	$('#countdown').html(currentTime);
 }
+
+$(document).ready(function() {
+	displayTime();
+	window.setInterval(function() {
+		tick();
+		displayTime();
+	}, 1000)
+});
