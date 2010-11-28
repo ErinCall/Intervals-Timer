60564e5738c2f49c2864c9d4ccf2fc9a030e42ae
Andrew Lorente <andrew.lorente@gmail.com>
2010-11-27 16:54:56 -0800

    Globals are unethical.
    (diff -w is clearer about what happened here)

--- countdown.js
+++ countdown.js
@@ -1,36 +1,38 @@
-var currentTime = 80;
-var timerIntervalId = 0;
-var script = [
-    4, 1,
-];
+(function() {
+    var currentTime = 80;
+    var timerIntervalId = 0;
+    var script = [
+        4, 1,
+    ];
 
-function tick() {
-    if (currentTime == 0 ) {
-        if (script.length > 0) {
-            intervalToggle();
-            currentTime = 600 * script.shift();
+    function tick() {
+        if (currentTime == 0 ) {
+            if (script.length > 0) {
+                intervalToggle();
+                currentTime = 600 * script.shift();
+            } else {
+                intervalCancel();
+                window.clearInterval(timerIntervalId)
+            }
         } else {
-            intervalCancel();
-            window.clearInterval(timerIntervalId)
+            currentTime--;
         }
-    } else {
-        currentTime--;
     }
-}
 
-function displayTime() {
-    var deciseconds = currentTime % 10;
-    var seconds = (currentTime/10) % 60;
-    var minutes = (currentTime/(60*10)) % 60;
-    $('#countdown').html(sprintf(
-        '%02d:%02d.%d', minutes, seconds, deciseconds
-    ));
-}
+    function displayTime() {
+        var deciseconds = currentTime % 10;
+        var seconds = (currentTime/10) % 60;
+        var minutes = (currentTime/(60*10)) % 60;
+        $('#countdown').html(sprintf(
+            '%02d:%02d.%d', minutes, seconds, deciseconds
+        ));
+    }
 
-$(document).ready(function() {
-    displayTime();
-    timerIntervalId = window.setInterval(function() {
-        tick();
+    $(document).ready(function() {
         displayTime();
-    }, 100)
-});
+        timerIntervalId = window.setInterval(function() {
+            tick();
+            displayTime();
+        }, 100)
+    });
+})();
